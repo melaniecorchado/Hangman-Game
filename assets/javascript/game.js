011d5433cb28f010ec7503226f9c96e38a4d78e0
Melanie Corchado <melaniecorchado@Melanies-MacBook-Pro.local>
2017-05-18 09:52:12 -0400

    updated files

---
+++ assets/javascript/game.js
@@ -0,0 +1,60 @@
+var dictionary = ['thailand', 'france', 'mexico', 'brazil', 'colombia'];
+var wordsList, input, placeHolder; 
+var totalGuesses = 10;
+
+    function startGame(){
+        placeHolder = ""; 
+        wordsList = dictionary[Math.floor(Math.random() * dictionary.length)];
+        console.log(wordsList);
+
+        var buttonText = document.getElementById("button").innerHTML = "Guess";
+
+        for(var i = 0; i < wordsList.length; i++){
+            placeHolder += "_";
+        }
+
+        document.getElementById("placeHolder").innerHTML = placeHolder;
+        document.getElementById("gamestatus").innerHTML = "Pack your bags and let's go!";
+
+    }
+
+        function guess(){
+            var correctGuess = false;
+            var ip = document.getElementById("letterGuess");
+            input = ip.value;
+
+            for(var i = 0; i < wordsList.length; i++)
+            {
+                if(input == wordsList.substring(i, i + 1)){
+                    correctGuess = true;
+                    placeHolder = placeHolder.substring(0, i) + input + placeHolder.substring(i + 1, placeHolder.length + 1);
+                    document.getElementById("placeHolder").innerHTML = placeHolder;
+                    console.log(placeHolder);
+                }
+            }
+
+    if(!correctGuess){
+        totalGuesses--;
+        document.getElementById("totalGuesses").innerHTML= totalGuesses;
+        console.log(totalGuesses);
+    }
+
+    if(placeHolder == wordsList){
+       alert("You WIN!");
+       startGame();
+        document.getElementById("button").onclick = guess;
+    }
+
+    if(totalGuesses == 0){
+       alert("You LOSE!")
+        startGame();
+        document.getElementById("button").onclick = guess
+    }
+
+
+        }
+
+
+
+startGame();
+document.getElementById("button").onclick = guess;
